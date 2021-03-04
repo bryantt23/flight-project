435dfcbcb438c8daa13f6241e3f08caf4f2354ff
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-03 22:45:20 -0800

    add passenger class

--- script.js
+++ script.js
@@ -8,9 +8,33 @@ class Passenger {
   get name() {
     return this._name;
   }
+
+  hasFlight(flightNumber) {
+    return this.flightNumbers
+      .map(flight => flight.toUpperCase())
+      .includes(flightNumber.toUpperCase());
+  }
+
+  addFlight(flightNumber) {
+    if (!this.hasFlight(flightNumber)) {
+      this.flightNumbers.push(flightNumber.toUpperCase());
+    }
+  }
 }
 
 const passenger = new Passenger('John');
 console.log(JSON.stringify(passenger));
 passenger.name = 'Jane';
+console.log(JSON.stringify(passenger));
+// passenger.flightNumbers = ['J512', 'AA128'];
+console.log(JSON.stringify(passenger));
+// console.log(passenger.hasFlight('J512'));
+// console.log(passenger.hasFlight('AA128'));
+// console.log(passenger.hasFlight('aa128'));
+// console.log(passenger.hasFlight('MB512'));
+// console.log(passenger.hasFlight('AA'));
+
+passenger.addFlight('aa128');
+console.log(JSON.stringify(passenger));
+passenger.addFlight('AA128');
 console.log(JSON.stringify(passenger));
